02c140e958e7ab3341d8b318a6180f9a09f043e3
System Administrator <zaagagon@gmail.com>
2022-03-31 23:45:45 -0500

    hola python

--- tabla2.py
+++ tabla2.py
@@ -3,5 +3,5 @@ print("cual es tu nombre")
 nombre=input()
 multiplicador = int(input(f'Cual tabla de multiplica deseas conocer {nombre}?'))
 print ("\n esta es la tabla del",multiplicador,)
-for j in range(1,13):
+for j in range(1,6):
     print("%d * %d = %d" % (multiplicador,j,multiplicador*j))
