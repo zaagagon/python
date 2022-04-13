7b6c538548d18f160845298674e76eebc3d550ee
System Administrator <zaagagon@gmail.com>
2022-04-13 00:10:25 -0500

    change branch master

--- funcion.py
+++ funcion.py
@@ -2,7 +2,7 @@
 #solucion rama master python defecto
 #ya no es main
 def main():
-    print('Introduce tu nombre')
+    print('Introduce tu nombre :')
     nombre = input()
     print(f'Bienvenido al mundo de {nombre}')
     
