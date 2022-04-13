02054d6b55869def28ab4856bb47e7ecb49d8312
System Administrator <zaagagon@gmail.com>
2022-04-12 23:24:13 -0500

    solucion python3

--- entrada.py
+++ entrada.py
@@ -1,8 +1,8 @@
 #programa que imprime 3 veces las letras
-#python entrada.py para ejecutar
-!#/usr/bin/python3/
-#teclado=input("diigite su nombre :")
-teclado="yolis"
+#python3 entrada.py para ejecutar
+
+teclado=input("diigite su nombre :")
+#teclado="yolis"
 print(f"hola, {teclado}")
 for i in range(3):
     print("a b c "
