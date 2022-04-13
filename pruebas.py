d829df5f4584e0c48927d78fae2822d4b0d47ae1
System Administrator <zaagagon@gmail.com>
2022-04-12 23:29:27 -0500

    compilacion python3

--- pruebas.py
+++ pruebas.py
@@ -1,9 +1,9 @@
-frutas = {1.35:"platano", 0.8:'Manzana',0.85:'Pera', 0.7:'Naranja'}
-precios = {8000:"bandeja paisa", 4500:"Desayuno paisa", 5500:"Ajiaco"}
+#ejecuta python3 pruebas.py
 
-accion = input('¿Qué fruta quieres? ').title()
+frutas = {'Plátano':1.35, 'Manzana':0.8, 'Pera':0.85, 'Naranja':0.7}
+fruta = input('¿Qué fruta quieres? ').title()
 kg = float(input('¿Cuántos kilos? '))
-if accion in frutas:
-    print(kg, 'kilos de', accion, 'valen', frutas[accion]*kg, '$')
+if fruta in frutas:
+    print(kg, 'kilos de', fruta, 'valen', frutas[fruta]*kg, '$')
 else: 
-    print("Lo siento, la fruta",accion, "no está disponible.")
+    print("Lo siento, la fruta", fruta, "no está disponible.")
