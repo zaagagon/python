d2329f830a3c4f4221acc47751c88f91d2ea0186
System Administrator <zaagagon@gmail.com>
2022-04-19 00:16:52 -0500

    revisar parsing python

--- suma.py
+++ suma.py
@@ -1,12 +1,14 @@
 #PROGRAMA QUE SUMA DOS NUMEROS EN PYTHON3
-from turtle import clear
+
 
 
 #limpiar pantalla
+
+#os.system("clear")
 teclado=input("Cual es un nombre : ")
 print("Bienvenid",teclado)
 print("**PROGRAMA QUE SUMA DOS NUMEROS**")
 N1=input("Digite primer numero :")
-N2=input("Numero 2 :")
+N2=input("Digite segundo numero :")
 suma=N1+N2
 print("Resultado de la suma :",suma)
