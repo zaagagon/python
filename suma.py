f4294dbbfe19a56476ec86c8daf10611bf362ee6
System Administrator <zaagagon@gmail.com>
2022-04-18 23:51:35 -0500

    funcion suma

--- suma.py
+++ suma.py
@@ -2,3 +2,6 @@ teclado=input("Cual es un nombre ")
 print("Bienvenid",teclado)
 print("**PROGRAMA QUE SUMA DOS NUMEROS**")
 N1=input("umero 1 :")
+N2=input("numero 2:")
+suma=N1+N2
+print("Resultado de la suma :")
