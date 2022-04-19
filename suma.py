2b28a9cfd58b5c2114e752fa1d13a593d05d595b
System Administrator <zaagagon@gmail.com>
2022-04-19 00:11:44 -0500

    commit numbers

--- suma.py
+++ suma.py
@@ -6,7 +6,7 @@ from turtle import clear
 teclado=input("Cual es un nombre : ")
 print("Bienvenid",teclado)
 print("**PROGRAMA QUE SUMA DOS NUMEROS**")
-N1=input("Numero 1 :")
+N1=input("Digite primer numero :")
 N2=input("Numero 2 :")
 suma=N1+N2
 print("Resultado de la suma :",suma)
