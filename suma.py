bda61474d1c7f88ce64995e9f1c84b3b0a933d1c
System Administrator <zaagagon@gmail.com>
2022-04-18 23:59:48 -0500

    xx

--- suma.py
+++ suma.py
@@ -1,7 +1,12 @@
-teclado=input("Cual es un nombre ")
+#PROGRAMA QUE SUMA DOS NUMEROS EN PYTHON3
+from turtle import clear
+
+
+#limpiar pantalla
+teclado=input("Cual es un nombre : ")
 print("Bienvenid",teclado)
 print("**PROGRAMA QUE SUMA DOS NUMEROS**")
-N1=input("umero 1 :")
-N2=input("numero 2:")
+N1=input("Numero 1 :")
+N2=input("Numero 2 :")
 suma=N1+N2
 print("Resultado de la suma :",suma)
