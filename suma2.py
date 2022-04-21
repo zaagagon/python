98560033a96bea532d1fd546bb8f3ead6ead34f9
System Administrator <zaagagon@gmail.com>
2022-04-21 00:07:16 -0500

    mejorando programa

--- suma2.py
+++ suma2.py
@@ -6,11 +6,13 @@ clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 clearConsole()
 
-input(" ** Programa que suma dos numeros **",'\n')
+print(" ** Programa que suma dos numeros **" + '\n')
 
 n1 = float(input("Digite número uno: "))
 n2 = float(input("Digite numero dos: "))
 suma = n1+n2
 print("Resultado de la suma es: ", suma)
+
+print(" ** fin del programa**" + '\n')
 
 
