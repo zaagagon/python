1a039386cd5837d1717685e17735053d6a9d309e
System Administrator <zaagagon@gmail.com>
2022-04-21 00:00:15 -0500

    suma dos numeros

--- suma2.py
+++ suma2.py
@@ -1,1 +1,11 @@
 #PROGRAMA QUE SUMA DOS NUMEROS EN PYTHON3
+
+input("Programa que suma dos numeros")
+
+n1 = float(input("Digite número uno: "))
+n2 = float(input("Digite numero dos: "))
+suma = n1+n2
+print("Resultado de la suma es: ", suma)
+n3 = float(input("la resuma: "))
+l = suma+n3
+print(" la resuma es: ", l)
