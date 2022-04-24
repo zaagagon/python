fcb7fd712461b4273e959aaac2140e88a4c2bbfa
System Administrator <zaagagon@gmail.com>
2022-04-23 23:15:41 -0500

    for py

--- multi.py
+++ multi.py
@@ -1,7 +1,12 @@
 #programa multi 2 numeros
+from turtle import clear, clearscreen
+
+
 print("multiverso")
 
+
 list=[3,4,5,6]
+print("Lista de numeros ***")
 for n in list:
     print
     print(n)
