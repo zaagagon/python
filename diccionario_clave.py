e92a34322962778e3fdef9f847031f7351f6e417
System Administrator <zaagagon@gmail.com>
2022-04-23 23:22:58 -0500

    diccionario.values

--- diccionario_clave.py
+++ diccionario_clave.py
@@ -1,4 +1,4 @@
-
+#dicionario clave /valor
 diccionario = {'P': 4, 'O': 3, 'L': 1, 'O': 0}
 for c in diccionario:
     print(c)
---
+++ diccionario_valor.py
@@ -0,0 +1,3 @@
+diccionario= {'P': 7, 'A': 7, 'L': 1, 'O': 4}
+for v in diccionario.values():
+    print(v)
