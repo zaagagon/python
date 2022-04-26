18c6b0761811994cc795027ed8fff662cc89249a
System Administrator <zaagagon@gmail.com>
2022-04-25 23:21:58 -0500

    diccionario prueba

--- diccionario4.py
+++ diccionario4.py
@@ -1,7 +1,13 @@
 #dicionario valores y claves
+from tkinter import N
+
+
 diccionario= {'P': 7, 'A': 7, 'L': 1, 'O': 4}
 #imprimamos valores
 for v in diccionario.values():
     print(v)
 
     #revisar#
+
+for n in diccionario :
+    print(n)
