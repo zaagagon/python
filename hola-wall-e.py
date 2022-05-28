bbaaa376857ab6990ab876fbb9f18c8b920ebf4d
System Administrator <zaagagon@gmail.com>
2022-05-28 16:58:38 -0500

    if en python3

--- hola-wall-e.py
+++ hola-wall-e.py
@@ -1,6 +1,14 @@
 #ejecutar python hola.py
+from telnetlib import theNULL
+
+
 print('Hola py en wall-e')
 
 a=9
 b=7
 print('resultado de la suma :' ,a+b)#concatenar datos
+
+arreglo = [1, 3, 4, 8]
+if 7 in arreglo:
+    print('está en valores')
+print('fin')
