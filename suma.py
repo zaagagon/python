a3914a2790f0398dbe15407c8ab5e604e160683b
System Administrator <zaagagon@gmail.com>
2022-07-07 10:41:01 -0500

    retomando python

--- suma.py
+++ suma.py
@@ -6,10 +6,11 @@
 
 #os.system("clear")
 teclado=input("Cual es un nombre : ")
-print("Bienvenid",teclado)
+print("Bienvenido",teclado)
 print("**PROGRAMA QUE SUMA DOS NUMEROS**")
 #int antes del input para que el dato sea entero
 N1=int(input("Digite primer numero :"))
 N2=int(input("Digite segundo numero :"))
 suma=N1+N2
 print("Resultado de la suma :",suma)
+print("Vuelve pronto")
