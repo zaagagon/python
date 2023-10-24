f9a990bdd9c4ecd2d65323db795c41b6d88dcf42
zaagagon <zaagagon@gmail.com>
2023-10-24 17:33:54 -0500

    mostrar el tamaño de arreglo en python

--- hola.py
+++ hola.py
@@ -6,3 +6,11 @@ b=7
 print('resultado de la suma :' ,a+b)#concatenar datos
 
 #ejecutar con python3
+
+# Crear un arreglo numérico de 5 posiciones
+mi_arreglo = [45, 52, 53, 34, 75]
+
+# Mostrar el arreglo en pantalla
+print(mi_arreglo)
+size = len(mi_arreglo)
+print(mi_arreglo)
