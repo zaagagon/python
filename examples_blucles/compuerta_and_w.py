937f0c7b879c26c1656a18be7229156a1bf6ddd0
zaagagon <zaagagon@gmail.com>
2023-10-24 20:27:43 -0500

    tabla and con f"

---
+++ examples_blucles/compuerta_and_w.py
@@ -0,0 +1,9 @@
+valor1 = 0 #false
+
+valor2 = 1 #true
+
+# Usando el operador "and"
+resultado = valor1 and valor2
+# Esto imprimirá "False" porque solo una de las dos expresiones es verdadera.
+print(f" valor1 | valor2 | resultado")
+print(f"   {valor1} |   {valor2} |   {resultado}")  
