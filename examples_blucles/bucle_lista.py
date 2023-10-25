00c95e66537a5e7a28bf2422e144299d5e55ce55
zaagagon <zaagagon@gmail.com>
2023-10-24 22:49:24 -0500

    bucle for lista cantidad elementeo

--- examples_blucles/bucle_lista.py
+++ examples_blucles/bucle_lista.py
@@ -1,5 +1,6 @@
 #creamos lista con 4 iteraciones
 lista =[3,45,6,7] 
+lista.insert(2,7)
 
 #recorremos la lista con el bucle for
 
--- examples_blucles/contador.py
+++ examples_blucles/contador.py
@@ -1,10 +1,21 @@
-lista = [2, "banana", 4, "manzana", 2]
+def mostrar(lista):
+    for elemento in lista:
+        print(elemento)
+
+lista = [2, 8, 4, 7, 2]
 contador = 0
+#insertar elemento 
+lista.insert(1, 2)
 
 
+    #muestra las iteraciones de la lista
+
+  
 
 for lista in lista:
-    if lista == 2:
-        contador += 1
+   if lista == 2:
+       contador += 1
 
 print("Cantidad de 2:", contador)
+
+mostrar(lista)
