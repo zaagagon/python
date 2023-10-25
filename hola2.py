03060b3e0986cf89cc8ed92bf7cf75219bd61a1a
zaagagon <zaagagon@gmail.com>
2023-10-25 00:06:03 -0500

    retomando funciones python suma

--- hola2.py
+++ hola2.py
@@ -4,3 +4,11 @@ print('Aprendiendo')
 a=9
 b=7
 print('resultado de la suma :' ,a+b)#concatenar datos
+
+#funcion suma 
+def suma(a,b):
+    return a+b
+
+print("resultado suma .. " , suma(9,8))
+nombre=input(print("como te llamas : "))
+print(f" bienvenido {nombre}")
