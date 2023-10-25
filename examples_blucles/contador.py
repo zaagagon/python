edeb39dbfc1441f6c29b8acd0f99bf2fce2d4746
zaagagon <zaagagon@gmail.com>
2023-10-25 00:00:46 -0500

    funcion hola

--- examples_blucles/contador.py
+++ examples_blucles/contador.py
@@ -2,11 +2,14 @@ def mostrar(lista):
     for elemento in lista:
         print(elemento)
 
+def hola(nombre):
+    print(f"hola {nombre}")
+
 lista = [2, 8, 4, 7, 2]
 contador = 0
 #insertar elemento 
 lista.insert(1, 2)
-
+hola("marcos")
 mostrar(lista)
 
   #muestra las iteraciones de la lista
