8e34e6d1724fbc22d2d6994eb8d6f9605ff603d6
zaagagon <zaagagon@gmail.com>
2023-10-24 19:59:47 -0500

    tabla and en python

---
+++ tabla_Y.py
@@ -0,0 +1,11 @@
+#entradas
+
+#creaamos dos liastas|
+A = [0,0,1,1]
+B = [0,1,0,1]
+
+print("A | B | A and B")
+
+for i in range(len(A))
+    result = A[i]and B[i]
+    print(f"{A[i]} | {B[i]} | {result})
