561543ebc9da7319b5b4ba6e4d5f8a98161b382b
zaagagon <zaagagon@gmail.com>
2024-01-29 23:55:13 -0500

    prueba 2024

---
+++ codificacion/arreglo_cambio.js
@@ -0,0 +1,9 @@
+// Declaramos un arreglo de colores
+let colores = ['rojo', 'verde', 'azul'];
+
+// Modificamos el segundo elemento (índice 1)
+colores[1] = 'amarillo';
+
+// Acceder al segundo elemento modificado
+const nuevoColor = colores[1];
+console.log('Nuevo color:', nuevoColor); // Resultado: 'amarillo'
---
+++ funcion3.py
@@ -0,0 +1,7 @@
+def saludar(nombre):
+    # Esta función toma un parámetro 'nombre' e imprime un saludo
+    print("Hola, " + nombre + "!")
+
+# Uso de la función
+nombre_usuario = input("Ingresa tu nombre: ")
+saludar(nombre_usuario)
