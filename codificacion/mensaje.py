d0a17841adaff5e80552cb7ee33f9fcaf84995b4
zaagagon <zaagagon@gmail.com>
2024-02-13 20:23:40 -0500

    codigo compartido

---
+++ codificacion/mensaje.py
@@ -0,0 +1,12 @@
+#mi primer ejemplo python
+
+#mi primer variable
+persona = "Diana"
+#mostrar un saludo
+print("Hola a todos bienvenidos");
+heroe="Viuda Negra"
+print( "Soy la .. " + heroe);
+print(persona);
+
+
+
--- funhola.py
+++ funhola.py
@@ -1,5 +1,10 @@
 def hola_mundo():
     print("¡Hola, Mundo!")
 
+num1 = 6
+num2 =5
+c = num1+num2
+print  (c)
+
 # Llamar a la función para imprimir "Hola, Mundo"
-hola_mundo()
+#hola_mundo()
