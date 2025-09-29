a6d36859851ca1783b0c6ea3ba9a89ebda1676d4
zaagagon <zaagagon@gmail.com>
2025-09-28 23:57:57 -0500

    promedio con pandas

--- pandas/promedio2.py
+++ pandas/promedio2.py
@@ -9,7 +9,7 @@ mensaje()
 lista=[3,4,5,6,3,2]
 
 promedio= pd.Series(lista)
-promedio2= pd.Series(lista).mean()
 print(promedio)
-promedio.serie.mean(lista)
+#promedio con pandas
+promedio2= pd.Series(lista).mean()
 print(promedio2)
