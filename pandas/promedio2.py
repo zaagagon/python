9a865180dbf733fbd593e6e4ad4122d69cded5ea
zaagagon <zaagagon@gmail.com>
2025-09-28 23:56:52 -0500

    pandas promedio

--- pandas/promedio2.py
+++ pandas/promedio2.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
 def mensaje():
-    print("hola")
+    print("hola ++ ")
     
 mensaje()
 
 
 lista=[3,4,5,6,3,2]
+
+promedio= pd.Series(lista)
+promedio2= pd.Series(lista).mean()
+print(promedio)
+promedio.serie.mean(lista)
+print(promedio2)
