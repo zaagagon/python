5858d415a018fcd77b15355bc9d5a2df87d548dc
zaagagon <zaagagon@gmail.com>
2025-09-29 00:08:06 -0500

    moda con pandas

--- pandas/promedio2.py
+++ pandas/promedio2.py
@@ -6,7 +6,7 @@ def mensaje():
 mensaje()
 
 
-lista=[3,4,5,6,3,2,7,8,]
+lista=[3,4,5,6,3,2,7,8,6,7,7,7]
 
 promedio= pd.Series(lista)
 print(promedio)
@@ -19,3 +19,4 @@ serie=pd.Series(lista)
 
 print("promedio con serie", serie.mean())
 print("mediana con serie", serie.median())
+print("moda con serie", serie.mode())
